eeb99fc32a1b11c0a3422504ddcffdb9131cb46d
Marv van Pal <marvvanpal@gmail.com>
2021-02-27 21:13:37 +0100

    Upload week2 assignment

--- week2/my_website.py
+++ week2/my_website.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, Week 2!</h1>'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/first-page')
+def first_page():
+    return render_template('first-page.html', page_title="First Template")
+
+
+@app.route('/second-page')
+def second_page():
+    return render_template('second-page.html', page_title="Second Template")
+
+# add additonal pages here using a similar format as above
 
 
 if __name__ == "__main__":
